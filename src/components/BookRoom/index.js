aeade057f12f88033431260d84066ec0a8d8679c
paz198787 <patrick.sjoberg@student.kyh.se>
2019-01-30 13:05:39 +0100

    working on grouprooms pushed to DB

--- src/components/BookRoom/index.js
+++ src/components/BookRoom/index.js
@@ -1,9 +1,100 @@
-import React from "react";
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { compose } from "recompose";
+import { Div } from "./styles";
+
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
 
 const BookRoom = () => (
-  <div>
-    <h1>BookRoom</h1>
-  </div>
+  <Div>
+    <h1>SignUp</h1>
+    <BookRoomForm />
+  </Div>
 );
+
+const INITIAL_STATE = {
+  groupRoom: "",
+  date: "",
+  time: ""
+};
+
+class BookRoomBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    const { groupRoom, date, time } = this.state;
+
+    this.props.firebase
+
+      .then(authUser => {
+        // Create a Grouproom in your Firebase realtime database
+        this.props.firebase
+
+          .groupRoom(authUser.user.uid)
+          .set({
+            groupRoom,
+            date,
+            time
+          })
+          .then(() => {
+            this.setState({ ...INITIAL_STATE });
+            this.props.history.push(ROUTES.HOME);
+          })
+          .catch(error => {
+            this.setState({ error });
+          });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { groupRoom, date, time } = this.state;
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="groupRoom"
+          value={groupRoom}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Group Rooms"
+        />
+        <input
+          name="date"
+          value={date}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Date"
+        />
+        <input
+          name="time"
+          value={time}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Time"
+        />
+        <button type="submit">Send to DB</button>
+      </form>
+    );
+  }
+}
+
+const BookRoomForm = compose(
+  withRouter,
+  withFirebase
+)(BookRoomBase);
 
 export default BookRoom;
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,45 +1,50 @@
-import app from 'firebase/app';
+import app from "firebase/app";
 
 import "firebase/database";
-import 'firebase/auth';
+import "firebase/auth";
 
 var config = {
-    apiKey: "AIzaSyAyiEnQ8kK74eH7UJ7BjsaHRoSZQcuqikU",
-    authDomain: "grex-6d87e.firebaseapp.com",
-    databaseURL: "https://grex-6d87e.firebaseio.com",
-    projectId: "grex-6d87e",
-    storageBucket: "grex-6d87e.appspot.com",
-    messagingSenderId: "1086033718659"
-  };
+  apiKey: "AIzaSyAyiEnQ8kK74eH7UJ7BjsaHRoSZQcuqikU",
+  authDomain: "grex-6d87e.firebaseapp.com",
+  databaseURL: "https://grex-6d87e.firebaseio.com",
+  projectId: "grex-6d87e",
+  storageBucket: "grex-6d87e.appspot.com",
+  messagingSenderId: "1086033718659"
+};
 
-  class Firebase {
-    constructor() {
-      app.initializeApp(config);
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
 
-      this.auth = app.auth();
-      this.db = app.database();
-    }
-
-        // *** Auth API ***
-      doCreateUserWithEmailAndPassword = (email, password) =>
-      this.auth.createUserWithEmailAndPassword(email, password);
-
-      doSignInWithEmailAndPassword = (email, password) =>
-      this.auth.signInWithEmailAndPassword(email, password);
-
-      doSignOut = () => this.auth.signOut();
-
-      doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-
-      doPasswordUpdate = password =>
-        this.auth.currentUser.updatePassword(password);
-
-    // *** User API ***
-
-    user = uid => this.db.ref(`users/${uid}`);
-
-    users = () => this.db.ref('users');
-  
+    this.auth = app.auth();
+    this.db = app.database();
   }
-  
-  export default Firebase;
+
+  // *** Auth API ***
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  createGrouproomWithDateAndTime = (groupRoom, date, time) =>
+    this.db.createGrouproomWithDateAndTime(groupRoom, date, time);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+
+  user = uid => this.db.ref(`users/${uid}`);
+
+  users = () => this.db.ref("users");
+
+  groupRoom = uid => this.db.ref(`groupRooms/${uid}`);
+
+  groupRooms = () => this.db.ref("groupRooms");
+}
+
+export default Firebase;
