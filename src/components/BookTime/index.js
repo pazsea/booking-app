1ce4c661a4098be1e1d72a69f18296654b7b5323
paz198787 <patrick.sjoberg@student.kyh.se>
2019-02-01 20:13:33 +0100

    book room sends to DB

--- src/components/BookTime/index.js
+++ src/components/BookTime/index.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
-class BookTime extends Component {
+import { compose } from "recompose";
+import { AuthUserContext, withAuthorization } from "../Session";
+import { withFirebase } from "../Firebase";
+
+const BookTime = props => (
+  <div>
+    <BookTimeComplete {...props} />
+  </div>
+);
+
+class BookTimeBase extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,37 +23,56 @@ class BookTime extends Component {
     this.setState({ [event.target.name]: event.target.checked });
   };
 
+  sendToDB = (event, authUser) => {
+    this.props.firebase.room().push({
+      ...this.state,
+      userId: authUser.uid
+    });
+    event.preventDefault();
+  };
+
   render() {
     const { close, bookDate, groupRoom } = this.props;
     return (
-      <React.Fragment>
-        <div>
-          <button onClick={close}>Close</button>
-          <p>{bookDate}</p>
-          <p>{groupRoom}</p>
-          <label>
-            <input
-              type="checkbox"
-              name="07:00-08:00"
-              onChange={this.onChangeCheckbox}
-            />
-            07:00-08:00
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="08:00-09:00"
-              onChange={this.onChangeCheckbox}
-            />
-            08:00-09:00
-          </label>
-        </div>
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <React.Fragment>
+            <div>
+              <button onClick={close}>Close</button>
+              <p>{bookDate}</p>
+              <p>{groupRoom}</p>
+              <label>
+                <input
+                  type="checkbox"
+                  name="07:00-08:00"
+                  onChange={this.onChangeCheckbox}
+                />
+                07:00-08:00
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  name="08:00-09:00"
+                  onChange={this.onChangeCheckbox}
+                />
+                08:00-09:00
+              </label>
+            </div>
 
-        <br />
-        <button>Send to DB</button>
-      </React.Fragment>
+            <br />
+
+            <button onClick={event => this.sendToDB(event, authUser)}>
+              Send to DB
+            </button>
+          </React.Fragment>
+        )}
+      </AuthUserContext.Consumer>
     );
   }
 }
 
-export default BookTime;
+const condition = authUser => !!authUser;
+
+const BookTimeComplete = withFirebase(BookTimeBase);
+
+export default compose(withAuthorization(condition))(BookTime);
