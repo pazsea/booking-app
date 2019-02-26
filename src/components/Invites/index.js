5d4d3673370497ef2168ff8d2c61fa6da2eb21b4
nina hedman <ni.hedman@gmail.com>
2019-02-26 22:40:34 +0100

    Resolved warnings and errors in BookTime, MyEvents, Invites, UpcommingEvents, BookRoom

--- src/components/Invites/index.js
+++ src/components/Invites/index.js
@@ -40,13 +40,8 @@ class InvitesBase extends Component {
           });
 
           const snapKeys = Object.keys(snap);
-          const test = snapKeys.map(key => {
+          snapKeys.forEach(key => {
             this.props.firebase.event(key).on("value", snapshot => {
-              /*               if (snapshot.val() === null) {
-                this.props.firebase
-                  .user(this.props.authUser.uid)
-                  .child("invitedToEvents")
-              } */
               const eventObject = snapshot.val();
               this.setState({
                 userEventObjects: [
@@ -55,6 +50,7 @@ class InvitesBase extends Component {
                 ]
               });
             });
+            return snap;
           });
         }
         this.setState({
