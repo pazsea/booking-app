8512820e26f237e1fbd98cb529d65ab8b951de63
nina hedman <ni.hedman@gmail.com>
2019-02-27 11:17:42 +0100

    Map: added attribution, accessToken, restructuring of code

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,29 +1,38 @@
 import React, { Component } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  width: ${props => props.width};
+  height: ${props => props.height};
+`;
 
-const { Map: LeafletMap } = window.ReactLeaflet (
+class Map extends Component {
+  componentDidMount() {
+    this.map = L.map("map", {
+      center: [58, 16],
+      zoom: 6,
+      zoomControl: false
+    });
 
-  
-  <div id="mapContainer">
-    <span />
-  </div>
+    L.tileLayer(
+      "https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png",
+      {
+        detectRetina: true,
+        maxZoom: 20,
+        maxNativeZoom: 17,
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        accessToken:
+          "pk.eyJ1IjoibmFpY2FoIiwiYSI6ImNqc2xsdGFxczJwYm40M254MnpnMGJlaHEifQ.CrjwoRsRgeGSWSFGC3xp6A"
+      }
+    ).addTo(this.map);
+  }
 
- 
-);
-
-
-// Make sure all the code (below) is called after the div and leaflet.js inclusion
-var mymap = L.map( 'mapContainer' ).setView( [ 51.505, -0.09 ], 13 );
-L.tileLayer( 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1IjoibmFpY2FoIiwiYSI6ImNqc2xsdGFxczJwYm40M254MnpnMGJlaHEifQ.CrjwoRsRgeGSWSFGC3xp6A}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox.streets',
-  accessToken: 'your.mapbox.access.token'
+  render() {
+    return <Wrapper width="1280px" height="720px" id="map" />;
+  }
 }
 
 export default Map;
-
-// Credit needs to be added: © OpenStreetMap contributors https://www.openstreetmap.org/copyright
-
-// Read about it at: https://www.openstreetmap.org/copyright
-// For a browsable electronic map, the credit should appear in the corner of the map. 
