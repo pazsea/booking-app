354949e80a38e113077123f08ce795535d0ebd10
paz198787 <patrick.sjoberg@student.kyh.se>
2019-02-27 11:28:28 +0100

    deleting

--- src/components/SignOut/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Nav } from "../Navigation/styles";
-import { withFirebase } from "../Firebase";
-
-const SignOutButton = ({ firebase }) => (
-  <Nav onClick={firebase.doSignOut}>Sign Out</Nav>
-);
-
-export default withFirebase(SignOutButton);
