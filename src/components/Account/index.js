69ee34c5c2766e724820116cd39bdc1cabb3417f
nina hedman <ni.hedman@gmail.com>
2019-03-03 16:56:08 +0100

    Cleanup Account - deleted import

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { AuthUserContext } from "../Session";
-import { PasswordForgetForm } from "../PasswordForget";
 import PasswordChangeForm from "../PasswordChange";
 import { withAuthorization } from "../Session";
 import { Div } from "./styles";
